9ec63c85dce190593c409f1d1543a7cb1fe75be2
Chris Meyers <cm.02.93@gmail.com>
2018-02-18 12:27:29 -0500

    cleaned up frequency output.

--- trumppet/cli.py
+++ trumppet/cli.py
@@ -99,6 +99,7 @@ def frequency():
     '''
 
     best_words = {}
+    largest_word_length = 0
     punctuation_to_keep = ['@', '#', '-']
     punctuation_to_remove = re.sub('[' + ''.join(punctuation_to_keep) + ']', '', string.punctuation)
     punctuation_to_remove = punctuation_to_remove + '“”…'
@@ -108,17 +109,21 @@ def frequency():
         if 'retweeted_status' in tweet:
             continue # Skip retweets
 
-        # Clean the tweet. Remove URLs, numbers and specified punctuation.
+        # Clean the tweet. Remove URLs, numbers, and specified punctuation.
         text = tweet['full_text'].lower().strip()
         text = re.sub(r'http\S+', '', text)
         text = re.sub(r'\d+', '', text)
         text = text.replace('...', ' ')
+        text = text.replace('\n', ' ')
         text = html.unescape(text)
         text = text.translate(translator)
 
         words = text.strip().split(' ')
 
         for word in words:
+            if len(word) > largest_word_length:
+                largest_word_length = len(word)
+
             if word == '' or word in punctuation_to_keep:
                 continue
             elif word in best_words:
@@ -132,7 +137,7 @@ def frequency():
     print('Here are his most frequent:')
     
     for word_info in best_words:
-        print(f'{word_info[0]}\t\t{word_info[1]}')
+        print(f'{word_info[0]: <{largest_word_length}} {word_info[1]}')
 
 
 # Setup available commands
