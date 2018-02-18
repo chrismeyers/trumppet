90d830bb03ff269ef6e54cb9d79c15537f23be6d
Chris Meyers <cm.02.93@gmail.com>
2018-02-18 11:25:18 -0500

    added frequency command.

--- trumppet/cli.py
+++ trumppet/cli.py
@@ -1,4 +1,8 @@
 import sys
+import string
+import re
+import html
+from operator import itemgetter
 import twitter
 import click
 import pymongo
@@ -38,12 +42,17 @@ def cli(context):
 
 @click.command()
 def record():
-    statuses = list(reversed(_api.GetUserTimeline(screen_name='realDonaldTrump')))
+    '''
+    Fetches the last 20 tweets and stores any that are new
+    '''
+
+    statuses = list(reversed(_api.GetUserTimeline(screen_name=_twitter_config['screen_name'])))
     new_tweets = False
 
+
     for status in statuses:
-        # Use the tweet id as the mongodb collection _id. This assumes we never get
-        # a duplicate tweet id. 
+        # Use the tweet id as the mongodb collection _id. This assumes we never 
+        # get a duplicate tweet id. 
         tweet = {
             '_id': status.id_str,
             'created_at': status.created_at,
@@ -55,6 +64,12 @@ def record():
             'urls': [{'expanded_url': url.expanded_url, 'url': url.url} for url in status.urls],
             'user_mentions': [{'id': user.id, 'screen_name': user.screen_name} for user in status.user_mentions]
         }
+
+        # If this is a retweet, just store the ID. The original tweet can be
+        # looked up later if needed.
+        if status.retweeted_status:
+            tweet['retweeted_status'] = {}
+            tweet['retweeted_status']['id'] = status.retweeted_status.id_str
 
         try:
             _db_tweets.insert_one(tweet)
@@ -69,13 +84,58 @@ def record():
 
 @click.command()
 def playback():
+    '''
+    Prints all stored tweets
+    '''
+
     for tweet in _db_tweets.find().sort("_id", pymongo.ASCENDING):
         print(f'[{tweet["created_at"]}] {tweet["full_text"]}\n')
+
+
+@click.command()
+def frequency():
+    '''
+    Gets a list of unique words in all tweets and a count of their occurrences
+    '''
+
+    best_words = {}
+    punctuation_to_keep = ['@', '#', '-']
+    punctuation_to_remove = re.sub('[' + ''.join(punctuation_to_keep) + ']', '', string.punctuation)
+    punctuation_to_remove = punctuation_to_remove + '“”…'
+    translator = str.maketrans('', '', punctuation_to_remove)
+
+    for tweet in _db_tweets.find().sort("_id", pymongo.ASCENDING):
+        # Clean the tweet. Remove URLs, numbers and specified punctuation.
+        text = tweet['full_text'].lower().strip()
+        text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text)
+        text = re.sub(r'\d+', '', text)
+        text = html.unescape(text)
+        text = text.translate(translator)
+
+        words = text.split(' ')
+
+        for word in words:
+            if word == '' or word in punctuation_to_keep:
+                continue
+            elif word in best_words:
+                best_words[word] += 1
+            else:
+                best_words[word] = 1
+
+    
+    best_words = sorted(best_words.items(), key=itemgetter(1), reverse=False)
+
+    print(f'@{_twitter_config["screen_name"]} really does have the best words!')
+    print('Here are his most frequent:')
+    
+    for word_info in best_words:
+        print(f'{word_info[0]}\t\t{word_info[1]}')
 
 
 # Setup available commands
 cli.add_command(record)
 cli.add_command(playback)
+cli.add_command(frequency)
 
 
 if __name__ == "__main__":
